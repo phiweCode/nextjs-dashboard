fb525a0d7b95222e00ee30d4e9477e183b05baf5
Siphiwe Mngoma <thabo.s.mngoma@gmail.com>
2025-01-14 14:41:38 +0200

    added card information

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,18 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';  
 import LatestInvoices from '../ui/dashboard/latest-invoices';
 import { lusitana } from '../ui/fonts'; 
-import { fetchRevenue, fetchLatestInvoices } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data'; 
+import { fetchCardData } from '../lib/data';
 
 
 const Page = async () => {  
+ 
 
+    const {
+        numberOfCustomers,
+        numberOfInvoices,
+        totalPaidInvoices, 
+        totalPendingInvoices,} = await fetchCardData() 
     const revenue = await fetchRevenue() 
     const latestInvoices = await fetchLatestInvoices()
 
@@ -17,14 +24,14 @@ const Page = async () => {
             </h1> 
 
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2xl"> 
-                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-                {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-                {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-                {/* <Card
+                <Card title="Collected" value={totalPaidInvoices} type="collected" /> 
+                <Card title="Pending" value={totalPendingInvoices} type="pending" />
+                <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> 
+                <Card
                 title="Total Customers"
                 value={numberOfCustomers}
                 type="customers"
-                /> */}
+                /> 
             </div> 
 
             <div className="mt-6 grid sm:grid-cols-2 lg:grid-cols-4">
